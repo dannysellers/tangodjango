3bcdceeb3de5b5b53229af5fbfa54e36ecf7ad52
danny <dsellers90@gmail.com>
2014-11-12 19:39:58 -0800

    linked db, added category routing/page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,16 +1,28 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from models import Category
+from models import Page
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 
 def index(request):
-	# Request the context of the request
+	# Obtain the context of the request
 	# The context contains info such as client's machine details, for example
 	context = RequestContext(request)
 
-	# Construct a dict to pass to the template engine as its content
-	# Note the key boldmessage is the same as {{ boldmessage }} in the template
-	context_dict = {'boldmessage': "I am bold font from the context"}
+	# Query the database for a list of all categories currently stored
+	# Order the categories by number of likes descending
+	# Retrieve the top 5, or all if < 5
+	# Place list in our context_dict to pass to template
+
+	# category_list = Category.objects.order_by('-likes')[:5]
+
+	category_list = Category.objects.order_by('name')[:5]
+	context_dict = {'categories': category_list}
+
+	# Replace spaces with underscores to retrieve URL
+	for category in category_list:
+		category.url = category.name.replace(' ', '_')
 
 	# Return a rendered response to send to the client
 	# The first param is the template to use
@@ -21,3 +33,35 @@ def about(request):
 	# return HttpResponse('Rango says: Here is the about page. <a href="/rango/">Home</a>')
 	context = RequestContext(request)
 	return render_to_response('rango/about.html', context)
+
+
+def category(request, category_name_url):
+	context = RequestContext(request)
+
+	# Change underscores in the category name to spaces
+	# The URL will have an underscore, which replaced with a space corresponds to the category
+	category_name = category_name_url.replace('_', ' ')
+
+	context_dict = {'category_name': category_name}
+
+	try:
+		# Can we find a category with the new given name?
+		# If not, the .get() method raises DoesNotExist exception
+		category = Category.objects.get(name=category_name)
+
+		# Retrieve all associated pages
+		# .filter method returns >= 1 model instance
+		pages = Page.objects.filter(category=category)
+
+		# Add results list to template context
+		context_dict['pages'] = pages
+
+		# We also add the category object from the database to the context directory
+		# We'll use this in the template to verify that the category exists
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		# We arrive here if no category is found
+		# Don't do anything, because the template displays "no category" if nothing
+		pass
+
+	return render_to_response('rango/category.html', context_dict, context)
