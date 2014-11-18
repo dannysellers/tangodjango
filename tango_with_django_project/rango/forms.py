b4e73f418423314fcc7bde2627bdf82e26f4ad62
danny <dsellers90@gmail.com>
2014-11-17 19:00:06 -0800

    undo commit test. pycharm's having issues pulling repo url

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -26,7 +26,7 @@ class PageForm(forms.ModelForm):
 		if url and not url.startswith('http://'):
 			url = 'http://' + url
 			cleaned_data['url'] = url
-		#change
+
 		return cleaned_data
 
 	class Meta:
